b8ef7fafedd3d3f62d73871f01404699cd095dfa
Agus221130642 <agussalim210420@gmail.com>
2024-02-21 23:36:58 +0700

    Update

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,204 @@
+import 'package:aplikasi_nonton_id/pages/register_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(28, 26, 41, 1),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 200.0),
+                child: _FormBody(),
+              ),
+            ),
+          ),
+          _FormAction(),
+        ],
+      ),
+    );
+  }
+}
+
+class _FormBody extends StatelessWidget {
+  const _FormBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'NONTON',
+              style: GoogleFonts.exo(
+                textStyle: const TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Text(
+              '\u25A0',
+              style: GoogleFonts.exo(
+                textStyle: const TextStyle(
+                  color: Color.fromRGBO(255, 222, 51, 1),
+                  fontSize: 12,
+                ),
+              ),
+            ),
+            Text(
+              'ID',
+              style: GoogleFonts.exo(
+                textStyle: const TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w900,
+                  color: Color.fromRGBO(255, 222, 51, 1),
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 20),
+        Text(
+          'Masuk',
+          style: GoogleFonts.openSans(
+            textStyle: const TextStyle(
+                fontSize: 20, fontWeight: FontWeight.w700, color: Colors.white),
+          ),
+        ),
+        const SizedBox(height: 30),
+        Container(
+          padding: const EdgeInsets.all(40.0),
+          child: Column(
+            children: [
+              TextFormField(
+                style: const TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  labelText: 'username',
+                  hintText: 'jhon doe',
+                  hintStyle: TextStyle(
+                    color: Colors.grey.shade800,
+                  ),
+                  fillColor: const Color.fromRGBO(47, 44, 68, 1),
+                  filled: true,
+                  contentPadding: const EdgeInsets.all(10.0),
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Icon(Icons.person_outline_rounded),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Required';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                style: const TextStyle(color: Colors.white),
+                keyboardType: TextInputType.name,
+                textInputAction: TextInputAction.done,
+                decoration: InputDecoration(
+                    labelText: 'password',
+                    hintText: '',
+                    fillColor: const Color.fromRGBO(47, 44, 68, 1),
+                    filled: true,
+                    contentPadding: const EdgeInsets.all(10.0),
+                    prefixIcon: const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Icon(Icons.lock),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    )),
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Text(
+                    'Belum Punya Akun?',
+                    style: GoogleFonts.openSans(
+                      textStyle: const TextStyle(
+                        fontSize: 12,
+                        color: Color.fromRGBO(255, 255, 255, 0.6),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    child: Text(
+                      'Daftar',
+                      style: GoogleFonts.openSans(
+                        textStyle: const TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w700,
+                          color: Color.fromRGBO(255, 255, 255, 1),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const RegisterPage(),
+                          ),
+                          (route) => false);
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _FormAction extends StatelessWidget {
+  const _FormAction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 90.0,
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+        child: Text(
+          'Masuk',
+          style: GoogleFonts.openSans(
+            textStyle:
+                const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+          ),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+}
---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,127 @@
+import 'package:aplikasi_nonton_id/pages/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        // Navigate to the login page after the delay
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const LoginPage(),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ShaderMask(
+          shaderCallback: (rect) => const LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.black, Colors.transparent],
+          ).createShader(rect),
+          blendMode: BlendMode.darken,
+          child: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/splash.jpg'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(Colors.black54, BlendMode.darken),
+              ),
+            ),
+          ),
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 200.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'NONTON',
+                      style: GoogleFonts.exo(
+                        textStyle: const TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w900,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '\u25A0',
+                      style: GoogleFonts.exo(
+                        textStyle: const TextStyle(
+                          color: Color.fromRGBO(255, 222, 51, 1),
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'ID',
+                      style: GoogleFonts.exo(
+                        textStyle: const TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w900,
+                          color: Color.fromRGBO(255, 222, 51, 1),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Center(
+                child: FutureBuilder<int>(
+                  future: Future.delayed(const Duration(seconds: 3), () => 1),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      // Transition to the login page after the delay
+                      return const SizedBox(); // Replace with navigation widget if needed
+                    } else {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const CircularProgressIndicator(),
+                          const SizedBox(height: 20.0),
+                          Text(
+                            'Loading...',
+                            style: GoogleFonts.openSans(
+                              textStyle: const TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
